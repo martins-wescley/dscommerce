88df11fe1a81b31354102c2bfa7bf3f7d23be2d0
Wescley Martins Sardinha Teixeira <wescleymartins@brq.com>
2023-09-19 10:55:15 -0300

    Added Role class and updated User class and import.sql

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -19,6 +19,12 @@ public class User {
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
     public User() {
     }
     public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
@@ -74,7 +80,6 @@ public class User {
         return password;
     }
 
-
     public String getUsername() {
         return email;
     }
@@ -85,6 +90,23 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role: roles) {
+            if (role.getAuthority().equals(roleName)){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
