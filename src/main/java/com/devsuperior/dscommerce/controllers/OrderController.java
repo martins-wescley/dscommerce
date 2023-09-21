39cc45ffcaadebf754c2f19a7adf1695782d254f
Wescley Martins Sardinha Teixeira <wescleymartins@brq.com>
2023-09-21 20:36:59 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dscommerce.controllers;
+
+import com.devsuperior.dscommerce.dto.OrderDTO;
+import com.devsuperior.dscommerce.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<OrderDTO>  findById(@PathVariable Long id) {
+        OrderDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+}
---
+++ src/main/java/com/devsuperior/dscommerce/repositories/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dscommerce.repositories;
+
+import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.projections.UserDetailsProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
